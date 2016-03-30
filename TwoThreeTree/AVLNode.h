d91e1c7543d72b84d29121b0772d64ee2b160a47
jdeleon <j.deleon6@umiami.edu>
2016-03-30 17:28:51 -0400

    Copied AVLTree as TwoThreeTree

---
+++ TwoThreeTree/AVLNode.h
@@ -0,0 +1,31 @@
+#ifndef AVLNODE_H_
+#define AVLNODE_H_
+
+#define nullHeight -1
+
+template <class T>
+struct AVLNode {
+	T data;
+	AVLNode *left;
+	AVLNode *right;
+	int height;
+	AVLNode(T data) {
+		this->data = data;
+		left = nullptr;
+		right = nullptr;
+		height = 0;
+	}
+	~AVLNode() {}
+	void evalHeight() {
+		int l = (left != nullptr) ? left->height : nullHeight;
+		int r = (right != nullptr) ? right->height : nullHeight;
+		height = max(l, r) + 1;
+	}
+	int getBalance() {
+		int l = (left != nullptr) ? left->height : nullHeight;
+		int r = (right != nullptr) ? right->height : nullHeight;
+		return (l - r);
+	}
+};
+
+#endif /* AVLNODE_H_ */
